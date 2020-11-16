17d9f789ba0b058fcfe35d45d824543ef714299b
nykim <kny0339@gmail.com>
2020-11-16 12:51:13 +0900

    1.4.2 오브젝트 팩토리의 활용

--- Springbook/src/main/java/com/springbook/practice/dao/DaoFactory.java
+++ Springbook/src/main/java/com/springbook/practice/dao/DaoFactory.java
@@ -3,8 +3,15 @@ package com.springbook.practice.dao;
 public class DaoFactory {
 	public UserDAO userDAO() {
 		//userdao 타입의 오브젝트를 어떻게 만들고, 어떻게 준비시킬지를 결정한다.
-		ConnectionMaker connectionMaker = new DConnectionMaker();
-		UserDAO dao = new UserDAO(connectionMaker);
-		return dao;
+//		ConnectionMaker connectionMaker = new DConnectionMaker();
+//		UserDAO dao = new UserDAO(connectionMaker);
+//		return dao;
+		
+//		connectionMaker 오브젝트를 반환하는 메소드를 따로 빼서 중복코드를 제거함(리팩토링)
+		return new UserDAO(connectionMaker());
+	}
+	
+	public ConnectionMaker connectionMaker() {
+		return new DConnectionMaker();
 	}
 }
