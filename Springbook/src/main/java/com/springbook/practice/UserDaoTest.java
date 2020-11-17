e468a969023d099c4fef38cc77c40c14e28d9e55
nykim <kny0339@gmail.com>
2020-11-17 12:53:14 +0900

    1.4.3 제어권의 이전을 통한 제어관계 역전까지 (스프링 적용: 애플리케이션 컨텍스트)

--- Springbook/src/main/java/com/springbook/practice/UserDaoTest.java
+++ Springbook/src/main/java/com/springbook/practice/UserDaoTest.java
@@ -2,6 +2,9 @@ package com.springbook.practice;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 import com.springbook.practice.dao.ConnectionMaker;
 import com.springbook.practice.dao.DConnectionMaker;
 import com.springbook.practice.dao.DaoFactory;
@@ -18,7 +21,14 @@ public class UserDaoTest {
 		//위 작업은 userDao와 connectionMaker 구현 클래스와의 런타임 오브젝트 의존관계를 설정하는 책임을 클라이언트로 가져온 결과이다.
 		
 		//팩토리를 사용해서 dao를 받아서 사용함
-		UserDAO dao = new DaoFactory().userDAO();
+//		UserDAO dao = new DaoFactory().userDAO();
+		
+		
+		//DaoFactory를 설정정보로 사용하는 애플리케이션 컨텍스트
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		
+		//첫번째 파라미터: cpplicationcontext에 등록된 빈의 이름(메소드 이름)
+		UserDAO dao = context.getBean("userDAO", UserDAO.class);
 		
 		User user = new User();
 		user.setId("wronggim");
--- Springbook/src/main/java/com/springbook/practice/dao/DaoFactory.java
+++ Springbook/src/main/java/com/springbook/practice/dao/DaoFactory.java
@@ -1,6 +1,12 @@
 package com.springbook.practice.dao;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration //애플리케이션 컨텍스트 또는 빈 팩토리가 사용할 설정정보라는 표시
 public class DaoFactory {
+	
+	@Bean //오브젝트 생성을 담당하는 IoC용 메소드라는 표시
 	public UserDAO userDAO() {
 		//userdao 타입의 오브젝트를 어떻게 만들고, 어떻게 준비시킬지를 결정한다.
 //		ConnectionMaker connectionMaker = new DConnectionMaker();
@@ -11,6 +17,7 @@ public class DaoFactory {
 		return new UserDAO(connectionMaker());
 	}
 	
+	@Bean
 	public ConnectionMaker connectionMaker() {
 		return new DConnectionMaker();
 	}
