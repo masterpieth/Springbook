717c68c0d93a27d8b05e9197e4db50c0ccb06566
nykim <kny0339@gmail.com>
2020-11-18 12:49:06 +0900

    1.6.2 싱글톤과 오브젝트의 상태(userDao)

--- Springbook/src/main/java/com/springbook/practice/dao/UserDAO.java
+++ Springbook/src/main/java/com/springbook/practice/dao/UserDAO.java
@@ -12,6 +12,8 @@ public class UserDAO {
 	
 //	private SimpleConnectionMaker simpleConnectionMaker;
 	private ConnectionMaker connectionMaker; //인터페이스를 통해 오브젝트에 접근하므로 구체적 클래스 정보를 알 필요가 없다.
+	private Connection c;
+	private User user;
 	
 	public UserDAO(ConnectionMaker connectionMaker) {
 //		simpleConnectionMaker = new SimpleConnectionMaker();
@@ -34,22 +36,28 @@ public class UserDAO {
 	
 	public User get(String id) throws ClassNotFoundException, SQLException {
 		
-		Connection c = connectionMaker.makeConnection();
+//		Connection c = connectionMaker.makeConnection();
+		this.c = connectionMaker.makeConnection();
 		
 		PreparedStatement ps = c.prepareStatement("select * from users where id = ?");
 		ps.setString(1, id);
 		
 		ResultSet rs = ps.executeQuery();
 		rs.next();
-		User user = new User();
-		user.setId(rs.getString("id"));
-		user.setName(rs.getString("name"));
-		user.setPassword(rs.getString("password"));
+//		User user = new User();
+//		user.setId(rs.getString("id"));
+//		user.setName(rs.getString("name"));
+//		user.setPassword(rs.getString("password"));
+		
+		this.user = new User();
+		this.user.setId(rs.getString("id"));
+		this.user.setName(rs.getString("name"));
+		this.user.setPassword(rs.getString("password"));
 		
 		rs.close();
 		ps.close();
 		c.close();
 		
-		return user;
+		return this.user;
 	}
 }
