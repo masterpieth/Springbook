7c12b29bc5efcf4e659d330ea9e8ec8fd69d9892
nykim <kny0339@gmail.com>
2021-01-21 12:59:26 +0900

    2021-01-21 트랜잭션 동기화

--- Springbook/src/main/java/com/springbook/practice/dao/UserService.java
+++ Springbook/src/main/java/com/springbook/practice/dao/UserService.java
@@ -1,6 +1,12 @@
 package com.springbook.practice.dao;
 
+import java.sql.Connection;
 import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.datasource.DataSourceUtils;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 import com.springbook.practice.domain.Level;
 import com.springbook.practice.domain.User;
@@ -8,7 +14,14 @@ import com.springbook.practice.domain.User;
 public class UserService {
 
 	UserDAO userDAO;
+	
 	UserLevelUpgradePolicy userLevelUpgradePolicy;
+	
+	private DataSource dataSource;
+	
+	public void setDataSource(DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 	
 	public void setUserDAO(UserDAO userDao) {
 		this.userDAO = userDao;
@@ -23,13 +36,28 @@ public class UserService {
 		userDAO.add(user);
 	}
 	
-	public void upgradeLevels() {
-		List<User> users = userDAO.getAll();
-		for(User user : users) {
-			if(canUpgradeLevel(user)) {
-				upgradeLevel(user);
+	public void upgradeLevels() throws Exception {
+		TransactionSynchronizationManager.initSynchronization();
+		Connection c = DataSourceUtils.getConnection(dataSource);
+		c.setAutoCommit(false);
+		
+		try {
+			List<User> users = userDAO.getAll();
+			for(User user : users) {
+				if(canUpgradeLevel(user)) {
+					upgradeLevel(user);
+				}
 			}
+			c.commit();
+		} catch (Exception e) {
+			c.rollback();
+			throw e;
+		} finally {
+			DataSourceUtils.releaseConnection(c, dataSource);
+			TransactionSynchronizationManager.unbindResource(this.dataSource);
+			TransactionSynchronizationManager.clearSynchronization();
 		}
+		
 	}
 	
 	protected boolean canUpgradeLevel(User user) {
